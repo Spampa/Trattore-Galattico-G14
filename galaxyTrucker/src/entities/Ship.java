007406ac8058a9477f46e9958787402d8b439bd7
P1vs <pivasimo3@gmail.com>
2025-04-01 15:08:28 +0200

    Update Ship.java

--- galaxyTrucker/src/entities/Ship.java
+++ galaxyTrucker/src/entities/Ship.java
@@ -2,9 +2,54 @@ package entities;
 
 public class Ship {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+	private float firePower;
+	private int motorPower;
+	private int waresValue;
+	private int voidConnectors;
+	private int humansCounter;
+	private int cellsCounter;
+	private int aliensCounter;
+	// private Components[][] shipComponents; TODO: implementare classe "Components"
+
+	public void scanShip(){
+		// TODO
+	}
+
+	public void getHit(){
+		// TODO
+	}
+
+	public boolean isPlayable(){
+		// TODO
+		return true;
+	}
+
+    public float getFirePower() {
+        return firePower;
+    }
+
+    public int getMotorPower() {
+        return motorPower;
+    }
+
+    public int getWaresValue() {
+        return waresValue;
+    }
+
+    public int getVoidConnectors() {
+        return voidConnectors;
+    }
+
+    public int getHumansCounter() {
+        return humansCounter;
+    }
+
+    public int getCellsCounter() {
+        return cellsCounter;
+    }
+
+    public int getAliensCounter() {
+        return aliensCounter;
 	}
 
 }
