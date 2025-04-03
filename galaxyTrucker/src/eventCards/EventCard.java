dded92ef4a707768c6b8e1429686a340af548c59
Nicolò Spampatti <nicolo.spampa@gmail.com>
2025-04-03 10:36:43 +0200

    Revert "Merge branch 'main' of https://github.com/Spampa/Trattore-Galattico-G14"
    This reverts commit d68a8cf4b0ce36f054bf7bcaa52942f8320a5dac, reversing
    changes made to f3ae1cf9dcb1e80ee65f9d2ff04585e43afdb4db.

--- galaxyTrucker/src/eventCards/EventCard.java
+++
@@ -1,9 +0,0 @@
-package eventCards;
-
-import gameEvents.Event;
-
-public interface EventCard {
-    Event generateEvent();  
-    String getName();      
-    String getDescription(); //ho messo la descrizione nel caso dovessimo fare la ui o qualcosa di grafico effettivamente.
-}
