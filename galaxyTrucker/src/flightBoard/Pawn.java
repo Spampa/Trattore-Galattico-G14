36da5fda11c5f4965bc0dc1203aeb47ad907c7fa
Andrei-Soava <andreisoava02@gmail.com>
2025-04-29 14:49:58 +0200

    Correzione errore

--- galaxyTrucker/src/flightBoard/Pawn.java
+++ galaxyTrucker/src/flightBoard/Pawn.java
@@ -1,71 +1,70 @@
-package entities;
+package flightBoard;
 
-public class Player {
-
-    private static int playerCount = 0;
-    private final int playerID;
-    private final Ship playerShip;
-
-    private int position;
-    private int generalPosition; 
-
-    public Player(Ship ship) throws Exception {
-        if (playerCount >= 4) {
-            throw new Exception("Massimo 4 giocatori consentiti.");
-        }
-
-        this.playerID = ++playerCount;
-        this.playerShip = ship;
-        this.position = 1;
-        this.generalPosition = 0;
-    }
-
-    public int getPlayerID() {
-        return playerID;
-    }
-
-    public Ship getPlayerShip() {
-        return playerShip;
-    }
-
-    public boolean checkPlayer() {
-        return playerShip.isPlayable();
-    }
-
-    public boolean isInGame() {
-        return checkPlayer();
-    }
-
-   
-    public int getPosition() {
-        return position;
-    }
-
-    public void setPosition(int position) {
-        this.position = position;
-    }
-
-    
-    public int getGeneralPosition() {
-        return generalPosition;
-    }
-
-    public void increaseGeneralPosition() {
-        this.generalPosition++;
-    }
-
-    public void decreaseGeneralPosition() {
-        this.generalPosition--;
-    }
-
-  
-    public void setStartingPosition(int startingPosition) {
-        this.generalPosition = startingPosition;
-    }
-
-   
-    public boolean isLappedBy(Player other) {
-        return other.getGeneralPosition() - this.getGeneralPosition() >= Board.NUM_OF_SPACES;
-    }
-
+public class Pawn {								
+	private static final int MAX_NUM_PLAYERS=4;
+	private static int numPlayers=0;				//initially 0, can go up to 4
+	private int position;							//indicates the position (1st, 2nd, 3rd, 4th)
+	private int generalPosition;					//indicates the number of spaces cleared (can be negative)
+	//TODO private final Color color;						
+	
+	public Pawn(/*Color color*/) {
+		if(numPlayers<=MAX_NUM_PLAYERS) {
+			this.generalPosition=0;
+			this.position=0;
+			//TODO this.color=color;
+			numPlayers++;
+		}
+		else
+			System.out.println("Number of players exceeded!");
+	}
+	
+	public void setStartingPosition(int numberOfArrival, Board board) {	//invoked only after ship-building-phase
+		generalPosition=MAX_NUM_PLAYERS-numberOfArrival;				//sets the position on the starting grid
+		position=numberOfArrival+1;										//sets the initial position of the player
+		board.getSpace(generalPosition-1).putPawn(this);				//fills out the space with corresponding player
+	}
+	
+	public void increaseGeneralPosition() {
+		this.generalPosition++;						//used in board when moving forward	
+	}
+	
+	public void decreaseGeneralPosition() {
+		this.generalPosition--;						//used in board when moving backwards
+	}
+	
+	public int getGeneralPosition() {
+		return this.generalPosition;
+	}
+	
+	public int getPosition() {
+		return this.position;
+	}
+	
+	public void setPosition(int p) {
+		this.position=p;
+	}
+	
+/*	public static void updatePosition(Player[] players) {
+		int[] temp=new int[players.length];					//static method which updates the position attribute of each player
+		for(int i=0;i<players.length-1;i++) {
+			for(int j=i+1;j<players.length;j++) {
+				if(players[i].getGeneralPosition()<players[j].getGeneralPosition())
+					temp[i]++;
+				else
+					temp[j]++;
+			}
+		}
+		
+		for(int t=0;t<players.length;t++) {
+			players[t].setPosition(temp[t]+1);
+		}			
+	}
+*/	
+	public boolean checkIfLapped(Pawn[] pawns) {		//method which checks if the player has been lapped
+		for(int i=0; i<pawns.length;i++) {
+			if(pawns[i].getGeneralPosition()-this.generalPosition>=Board.NUM_OF_SPACES)
+				return true;
+		}
+		return false;		
+	}	
 }
